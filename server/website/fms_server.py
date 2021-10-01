174702f3871c117c4155a698129e4575ce3004e2
Dal Brandon <dalbrandon@gmail.com>
2021-09-30 20:30:57 -0700

    work at school

--- server/website/fms_server.py
+++ server/website/fms_server.py
@@ -1,8 +1,6 @@
 # fms_server.py -- Main Server Program File for EpicFms Server
 # dlb, Sept 2021
 #
-# 
-#
 
 from flask import Flask, render_template
 from threading import Event
@@ -11,25 +9,16 @@ import timercb
 import score_manager as sm
 import time
 
-game_status = {
-      'team_a' : '---',
-      'team_b': '---', 
-      'etime' : '---'
-}
 loopcount = 0
 score_man  = sm.ScoreManager() 
 score_man.update()
+dummy_params = { "dummy" : 0}
 
 # Main loop here.  Does work that is not the web server.
 def mainloop():
     global game_status, loopcount, score_man
     score_man.update()
-
     loopcount += 1
-    game_status['etime'] = '%d Seconds' % loopcount
-    if loopcount == 20:
-        game_status['team_a'] = 'babies'
-        game_status['team_b'] = 'soliders'
 
 doneevent = Event()
 looptimer_thread = timercb.TimerCB(mainloop, 0.100, doneevent)
@@ -43,16 +32,15 @@ def hello():
    now = datetime.datetime.now()
    timeString = now.strftime("%Y-%m-%d %H:%M")
    stuff = {'title' : 'Hi There'}
-   return render_template('index.html', **game_status)
+   return render_template('index.html', **dummy_params)
 
 @webapp.route("/score")
 def score():
-   templateData = {
-      'team_a' : 'elders',
-      'team_b': 'bad boys', 
-      'etime' : '45 Secs'
-      }
-   return render_template('scoreboard.html', **game_status)
+   return render_template('scoreboard.html', **dummy_params)
+
+@webapp.route("/admin")
+def admin():
+  return render_template('admin.html', **dummy_params)
 
 # Rawscore returns JSON text that describes the complete state of the game
 @webapp.route("/rawscore")
